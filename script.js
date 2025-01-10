76004934a9218accddec4241d4fd313a7c44ef22
Andrei Nitu <andreiraul.nitu@gmail.com>
2025-01-10 20:23:26 +0200

    Add buttons to change background colour.

--- script.js
+++ script.js
@@ -1,3 +1,82 @@
+//code for the map -> used the code that has been taught at the seminar.
+window.onload = function () {
+    navigator.geolocation.getCurrentPosition(onLocationSuccess, onLocationError)
+
+    function onLocationSuccess(position) {
+        console.log(position.coords.accuracy);
+        console.log(position);
+
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
+        let accuracy = position.coords.accuracy;
+
+        let map = L.map('map').setView([lat, long], 17);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: 'Multimedia'
+        }).addTo(map);
+
+        L.marker([lat, long]).addTo(map);
+
+        L.circle([lat, long], {
+            color: 'red',
+            fillColor: '#ff0033',
+            fillOpacity: 0.5,
+            radius: accuracy
+        }).addTo(map);
+        //afisare longitudine si latitudine langa harta
+        document.getElementById('long').textContent = "Longitudine: " + long;
+        document.getElementById('lat').textContent = "Latitudine: " + lat;
+    }
+
+    function onLocationError(error) {
+        console.log(error);
+    }
+
+
+}
+
+//functions to get the hour of each city
+function getCityTime(offset) {
+    const now = new Date();
+    const utc = now.getTime() + now.getTimezoneOffset() * 60000; // UTC hour, found by using Romania's hour. Multip by 60000 to get milliseconds
+    const cityTime = new Date(utc + (3600000 * offset));//get the hour for each city based on the UTC and the offset of each city
+    const hour = String(cityTime.getHours()).padStart(2, '0');//padstart used to format the time. 
+    const minute = String(cityTime.getMinutes()).padStart(2, '0');
+    const second = String(cityTime.getSeconds()).padStart(2, '0');
+    return `${hour}:${minute}:${second}`;
+}
+
+//function to "draw" each hour for each country based on the offset. 
+//Offset will be hard coded when the functions will be called.
+function drawCityTime(cityId, offset) {
+    const cityCanvas = document.getElementById(cityId);
+    const ctxCity = cityCanvas.getContext("2d");
+
+    const width = cityCanvas.width;
+    const height= cityCanvas.height;
+    
+    const time = getCityTime(offset);
+
+    ctxCity.clearRect(0, 0, width, height);
+
+
+    ctxCity.font = "40px Arial Black";
+    ctxCity.textAlign = "center";
+    ctxCity.textBaseline = "middle";
+    ctxCity.fillText(time, width/2, height/2);
+}
+
+//used to update each clock + the hard coded offset of each city.
+setInterval(() => {
+    drawCityTime("LondonTime", 0); 
+    drawCityTime("NYTime", -5);  
+    drawCityTime("ParisTime", 1);
+    drawCityTime("BeijingTime", 8); 
+}, 1000);//1000 used just like it has been used for the big clock, to "reload" the canvas
+
+//main clock time
 const tick = document.getElementById('tickingSound')
 const soundButton = document.getElementById('buttonForSound')
 let isSoundOn = false // to control the sound -> initially, the sound is NOT on
@@ -55,4 +134,53 @@ function playTick(){
     tick.play();
 }
 
-setInterval(playTick, 100);
+setInterval(playTick, 75);
+
+//buttons for theme change
+const redButton = document.getElementById('Red')
+const blueButton = document.getElementById('Blue')
+const GreenButton = document.getElementById('Green')
+const defButton = document.getElementById('Default')
+//function to change theme to red
+function changeThemeRed(){
+    document.body.style.backgroundColor = 'red';
+    document.getElementById('clockDiv').style.backgroundColor = 'red';
+}
+redButton.addEventListener('click', changeThemeRed);
+
+//function to change theme to blue
+function changeThemeBlue(){
+    document.body.style.backgroundColor = 'blue';
+    document.getElementById('clockDiv').style.backgroundColor = 'blue';
+}
+blueButton.addEventListener('click', changeThemeBlue);
+
+//function to change theme to green
+function changeThemeGreen(){
+    document.body.style.backgroundColor = 'green';
+    document.getElementById('clockDiv').style.backgroundColor = 'green';
+}
+GreenButton.addEventListener('click', changeThemeGreen);
+
+//functon to change theme back to default value
+function changeToDefault(){
+    document.body.style.backgroundColor = '#F5F5DC';
+    document.getElementById('clockDiv').style.backgroundColor = '#F5F5DC';
+}
+defButton.addEventListener('click', changeToDefault);
+
+const HEXbutton = document.getElementById('HEXbutton');
+const colourChange = document.getElementById('colorInput');
+
+function changeColourHEX(){
+    newColour = colourChange.value;
+    if(newColour.startsWith('#') && newColour.length == 7){
+        document.body.style.backgroundColor = newColour;
+        document.getElementById('clockDiv').style.backgroundColor = newColour;
+    }
+    else{
+    alert('Input a valid HEX code.')
+    }
+}
+
+HEXbutton.addEventListener('click', changeColourHEX);   
